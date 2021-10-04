b350bcd22e0788fae258e413653f339b41df9009
Geir Sagberg <geir.sagberg@gmail.com>
2021-10-04 15:45:31 +0200

    Update saga-build plugin version in example

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.31"
-    id("saga-build") version "1.1.11"
+    id("saga-build") version "1.1.5"
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 

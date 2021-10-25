8ee65465a87d17d51c378b45a71f0dcc76de998a
Marius B. Kotsbak <marius.kotsbak@webstep.no>
2021-10-25 09:36:24 +0200

    Extend Env utils with Arrow's Validated support instead of throwing
    Add envToValidated and envToValidatedNel for handling the case when multiple env variables are required and we should report all missing variables as error message.
    
    Add tests that verify functionality and shows how to use them together with zip.

--- modules/shared/src/main/kotlin/no/vegvesen/saga/modules/shared/Env.kt
+++ modules/shared/src/main/kotlin/no/vegvesen/saga/modules/shared/Env.kt
@@ -1,4 +1,12 @@
 package no.vegvesen.saga.modules.shared
 
-fun envOrThrow(envVar: String) =
-    System.getenv(envVar) ?: throw Error("Missing environment variable $envVar")
+import arrow.core.Validated
+import arrow.core.ValidatedNel
+
+fun envOrThrow(envVar: String): String =
+    envToValidated(envVar).mapLeft { Error(it) }.toEither().getOrThrow()
+
+fun envToValidated(envVar: String): Validated<String, String> =
+    Validated.fromNullable(System.getenv(envVar)) { "Missing environment variable $envVar" }
+
+fun envToValidatedNel(envVar: String): ValidatedNel<String, String> = envToValidated(envVar).toValidatedNel()

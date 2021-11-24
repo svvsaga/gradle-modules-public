9c1ff5509dff5fc29bb777ba85d933f653e96300
Geir Sagberg <geir.sagberg@gmail.com>
2021-11-24 10:27:23 +0100

    Use external lint-staged-config

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,10 +1,3 @@
-module.exports = {
-  '**/*.kt?(s)': () => ['ktlint -F'],
-  '**': (files) => [
-    // There are some files in github actions that trigger the secrets scan, false alarm. Also some large JS bundles.
-    'scripts/check-for-secrets.ts ' +
-      files.filter((file) => !file.includes('.github/actions')).join(' '),
-    'scripts/block-large-files.ts ' +
-      files.filter((file) => !file.includes('.github/actions')).join(' '),
-  ],
-}
+module.exports = require('node-modules-public').lintStagedConfig({
+  ktlint: true,
+})

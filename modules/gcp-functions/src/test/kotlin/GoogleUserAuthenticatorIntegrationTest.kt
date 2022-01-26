e5627b03d91fe46f4444c77a367d1cb34db6e794
Marius B. Kotsbak <marius.kotsbak@webstep.no>
2022-01-26 16:18:51 +0100

    GoogleUserAuthenticatorIntegrationTest: Set as ExternalTest to ignore automatic integration testing

--- modules/gcp-functions/src/test/kotlin/GoogleUserAuthenticatorIntegrationTest.kt
+++ modules/gcp-functions/src/test/kotlin/GoogleUserAuthenticatorIntegrationTest.kt
@@ -7,12 +7,16 @@ import io.kotest.matchers.string.shouldNotBeBlank
 import io.mockk.every
 import io.mockk.mockk
 import no.vegvesen.saga.modules.gcp.functions.GoogleUserAuthenticator
+import no.vegvesen.saga.modules.testing.ExternalTest
 import no.vegvesen.saga.modules.testing.IntegrationTest
 import no.vegvesen.saga.modules.testing.shouldBeRightAnd
 import java.util.Optional
 
-// NOTE: Requires having logged in with a user as GOOGLE_APPLICATION_CREDENTIALS (or `gcloud auth application-default login`)
-@Tags(IntegrationTest)
+/*
+ NOTE: Requires having logged in with a user as GOOGLE_APPLICATION_CREDENTIALS (or `gcloud auth application-default login`)
+ Set as ExternalTest to ignore automatic integration testing, auth not working from GihHub (not implementing IdTokenProvider)
+ */
+@Tags(IntegrationTest, ExternalTest)
 class GoogleUserAuthenticatorIntegrationTest : FunSpec({
     val testSubject = GoogleUserAuthenticator()
 

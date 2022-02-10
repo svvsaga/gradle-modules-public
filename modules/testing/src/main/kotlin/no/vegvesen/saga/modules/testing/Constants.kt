dcf11fc031f8b33c2970ee6c6f160e161ebd4668
Marius B. Kotsbak <marius.kotsbak@webstep.no>
2022-02-10 13:32:59 +0100

    Handle null when no SAGA_INT_TEST_PROJECT_ID set #patch

--- modules/testing/src/main/kotlin/no/vegvesen/saga/modules/testing/Constants.kt
+++ modules/testing/src/main/kotlin/no/vegvesen/saga/modules/testing/Constants.kt
@@ -1,3 +1,5 @@
 package no.vegvesen.saga.modules.testing
 
-val SagaIntTestProject: String by lazy { System.getenv("SAGA_INT_TEST_PROJECT_ID") }
+import no.vegvesen.saga.modules.shared.envOrThrow
+
+val SagaIntTestProject: String by lazy { envOrThrow("SAGA_INT_TEST_PROJECT_ID") }

034fef0e8adf4efd8ab12dd752b0a9236c7d25ed
Geir Sagberg <geir.sagberg@gmail.com>
2022-03-09 12:54:44 +0100

    Add more testing helpers for reading resources #minor

--- modules/testing/src/main/kotlin/no/vegvesen/saga/modules/testing/ResourceHelpers.kt
+++ modules/testing/src/main/kotlin/no/vegvesen/saga/modules/testing/ResourceHelpers.kt
@@ -1,5 +1,9 @@
 package no.vegvesen.saga.modules.testing
 
-fun Any.loadStringResourceOrThrow(file: String): String =
-    javaClass.getResourceAsStream(if (file.startsWith('/')) file else "/$file")?.readBytes()?.toString(Charsets.UTF_8)
-        ?: throw Exception("No resource '$file' exists on classpath")
+fun Any.loadStringResourceOrThrow(file: String): String = readResourceAsBytes(file).toString(Charsets.UTF_8)
+
+fun Any.readResourceAsBytes(file: String): ByteArray = readResourceAsStream(file).readBytes()
+
+fun Any.readResourceAsStream(file: String) =
+    javaClass.getResourceAsStream(if (file.startsWith('/')) file else "/$file")
+        ?: throw Exception("Missing resource: $file")

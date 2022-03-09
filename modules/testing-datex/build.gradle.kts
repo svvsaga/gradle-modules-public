2cb31af044e3fed0b3b08a7a233bb02b4e65e103
Geir Sagberg <geir.sagberg@gmail.com>
2022-03-09 15:08:09 +0100

    Add integration tests for all Datex endpoints
    Also remove testing-datex as it is no longer needed #major

--- modules/testing-datex/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencies {
-    implementation(project(":datex"))
-    implementation(project(":testing"))
-    implementation(project(":gcp"))
-    implementation(project(":gcp-storage"))
-    implementation(project(":ktor"))
-    implementation("io.ktor:ktor-client-mock-jvm")
-    implementation("com.google.cloud", "google-cloud-storage")
-}

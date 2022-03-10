4aa41eff15b126de783f9e9331edca4afdf7d3a1
Geir Sagberg <geir.sagberg@gmail.com>
2022-03-10 10:37:05 +0100

    Add generic DatexPollerFunction
    I noticed we have almost duplicate code for all our datex pollers. This addition generifies the function. #minor

---
+++ modules/datex-poller/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    implementation(project(":datex"))
+    implementation(project(":gcp"))
+    implementation(project(":gcp-functions"))
+    implementation(project(":gcp-storage"))
+    implementation(project(":gcp-datastore"))
+    implementation(project(":ktor"))
+}

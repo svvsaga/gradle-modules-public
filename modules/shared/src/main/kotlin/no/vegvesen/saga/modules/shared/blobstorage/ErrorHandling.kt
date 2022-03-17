030953b4ce1b18de6ad55a400fe2dc0a30358efd
Geir Sagberg <geir.sagberg@gmail.com>
2022-03-17 13:44:02 +0100

    Add handleBlobNotFound #minor

---
+++ modules/shared/src/main/kotlin/no/vegvesen/saga/modules/shared/blobstorage/ErrorHandling.kt
@@ -0,0 +1,22 @@
+package no.vegvesen.saga.modules.shared.blobstorage
+
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import no.vegvesen.saga.modules.shared.kv
+import org.slf4j.LoggerFactory
+
+private val log = LoggerFactory.getLogger("BlobStorage")
+
+/**
+ * Log warning when blob is not found, but return Right.
+ */
+fun <A, B> Either<A, B>.handleBlobNotFound(path: StoragePath): Either<A, Unit> = fold({
+    if (it is BlobStorageError.BlobNotFound) {
+        log.warn(
+            "File not found, potentially already processed because of PubSub at-least-once semantics",
+            kv("path", path)
+        )
+        Unit.right()
+    } else it.left()
+}, { Unit.right() })

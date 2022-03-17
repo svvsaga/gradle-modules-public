6f98fb9946991476dd753fb1c7c5a63e6091829f
Geir Sagberg <geir.sagberg@gmail.com>
2022-03-17 16:53:53 +0100

    Read storage path from BlobNotFound exception #patch

--- modules/shared/src/main/kotlin/no/vegvesen/saga/modules/shared/blobstorage/ErrorHandling.kt
+++ modules/shared/src/main/kotlin/no/vegvesen/saga/modules/shared/blobstorage/ErrorHandling.kt
@@ -11,11 +11,11 @@ private val log = LoggerFactory.getLogger("BlobStorage")
 /**
  * Log warning when blob is not found, but return Right.
  */
-fun <A, B> Either<A, B>.handleBlobNotFound(path: StoragePath): Either<A, Unit> = fold({
+fun <A, B> Either<A, B>.handleBlobNotFound(): Either<A, Unit> = fold({
     if (it is BlobStorageError.BlobNotFound) {
         log.warn(
             "File not found, potentially already processed because of PubSub at-least-once semantics",
-            kv("path", path)
+            kv("path", it.storagePath)
         )
         Unit.right()
     } else it.left()

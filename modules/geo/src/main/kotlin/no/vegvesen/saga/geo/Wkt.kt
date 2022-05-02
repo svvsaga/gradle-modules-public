997ecd04a8985b749c0585cadd1b95bf53780598
Geir Sagberg <geir.sagberg@gmail.com>
2022-05-02 15:56:24 +0200

    Extend and replace WKT helper utils with more functionality #major
    Add handling of collapsing invalid geometry, also flip XY optionally. Needed for BQ.

--- modules/geo/src/main/kotlin/no/vegvesen/saga/geo/Wkt.kt
+++ modules/geo/src/main/kotlin/no/vegvesen/saga/geo/Wkt.kt
@@ -1,13 +1,32 @@
 package no.vegvesen.saga.geo
 
 import arrow.core.Either
+import org.locationtech.jts.geom.CoordinateFilter
 import org.locationtech.jts.io.WKTReader
 
 class Wkt {
     companion object {
-        fun removeZCoordinates(wkt: String) =
-            WKTReader().let {
-                Either.catch { it.read(wkt).toText() }
-            }
+        private val wktReader = WKTReader()
+
+        private val flipXyCoordinatesFilter = CoordinateFilter { coordinate ->
+            val x = coordinate.x
+            coordinate.x = coordinate.y
+            coordinate.y = x
+        }
+
+        /**
+         * Converts 3D coordinates to 2D coordinates, collapses invalid geometry to POINT, and optionally flips X and Y.
+         */
+        fun prepareForBigQuery(wkt: String, flipXy: Boolean) = Either.catch {
+            wktReader.read(wkt)
+                .let { if (it.isValid) it else it.centroid }
+                .also {
+                    if (flipXy) {
+                        it.apply(flipXyCoordinatesFilter)
+                        it.geometryChanged()
+                    }
+                }
+                .toText()
+        }
     }
 }

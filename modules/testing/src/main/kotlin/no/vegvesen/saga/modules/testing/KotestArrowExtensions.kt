6a07ca70533e8d77ee84bf7a9e113bb8ef811b69
Geir Sagberg <geir.sagberg@gmail.com>
2022-05-30 14:08:47 +0200

    Add kotest extensions for mocking eithers #minor

--- modules/testing/src/main/kotlin/no/vegvesen/saga/modules/testing/KotestArrowExtensions.kt
+++ modules/testing/src/main/kotlin/no/vegvesen/saga/modules/testing/KotestArrowExtensions.kt
@@ -2,11 +2,15 @@ package no.vegvesen.saga.modules.testing
 
 import arrow.core.Either
 import arrow.core.Validated
+import arrow.core.left
+import arrow.core.right
 import io.kotest.assertions.arrow.core.shouldBeInvalid
 import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.assertions.arrow.core.shouldBeValid
 import io.kotest.matchers.types.shouldBeTypeOf
+import io.mockk.ConstantAnswer
+import io.mockk.MockKStubScope
 
 inline infix fun <A, B> Either<A, B>.shouldBeRightAnd(f: (b: B) -> Unit): B =
     shouldBeRight().also { f(it) }
@@ -23,3 +27,9 @@ inline infix fun <A, B> Validated<A, B>.shouldBeInvalidAnd(f: (a: A) -> Unit): A
 inline fun <reified T : Any> Either<Any?, Any?>.shouldBeLeftOfType() = shouldBeLeft().shouldBeTypeOf<T>()
 
 inline fun <reified T : Any> Either<Any?, Any?>.shouldBeRightOfType() = shouldBeRight().shouldBeTypeOf<T>()
+
+infix fun <T, B, L> MockKStubScope<Either<L, T>, B>.returns(returnValue: T) =
+    answers(ConstantAnswer(returnValue.right()))
+
+infix fun <T, B, L> MockKStubScope<Either<L, T>, B>.returnsLeft(returnValue: L) =
+    answers(ConstantAnswer(returnValue.left()))

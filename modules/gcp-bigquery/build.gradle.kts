30d637071312409664dfbe626009a182b3a5e8bc
Geir Sagberg <geir.sagberg@gmail.com>
2022-08-09 14:48:41 +0200

    Remove explicit dependency on big-query storage version (covered by BOM) #patch

--- modules/gcp-bigquery/build.gradle.kts
+++ modules/gcp-bigquery/build.gradle.kts
@@ -3,6 +3,6 @@ dependencies {
     testImplementation(project(":testing"))
     testImplementation(project(":testing-gcp"))
     api("com.google.cloud", "google-cloud-bigquery")
-    api("com.google.cloud", "google-cloud-bigquerystorage", "2.12.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.3.2")
+    api("com.google.cloud", "google-cloud-bigquerystorage")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.3.3")
 }

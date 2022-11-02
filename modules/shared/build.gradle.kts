svvsaga/gradle-modules-public

commit 97f090018f85bc68a706cca16078dd2ffcf9fe23

Update versions of Beam, logback, slf4j #major

modules/shared/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     api("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("ch.qos.logback:logback-classic:1.4.3")
     implementation("org.jetbrains.kotlinx", "kotlinx-serialization-properties")
 
     testImplementation(project(":testing"))
@@ -10,10 +10,10 @@ dependencies {
     implementation("com.fasterxml.jackson.core", "jackson-databind")
     implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jdk8")
     implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310")
-    // should not have be exposed ie. "implementation", see Kanbanize: #3507
-    api("org.slf4j:slf4j-api:1.7.36")
+
+    api("org.slf4j:slf4j-api:2.0.3")
     api("net.logstash.logback:logstash-logback-encoder:7.2")
-    api("org.buildobjects:jproc:2.8.0")
+    api("org.buildobjects:jproc:2.8.2")
 
     api("org.jetbrains.kotlinx", "kotlinx-datetime")
 }

modules/beam/build.gradle.kts
@@ -1,4 +1,4 @@
-val beamVersion = "2.40.0"
+val beamVersion = "2.41.0"
 
 dependencies {
     api("org.apache.beam", "beam-runners-google-cloud-dataflow-java", beamVersion)
@@ -6,7 +6,7 @@ dependencies {
     api("org.apache.beam", "beam-sdks-java-io-google-cloud-platform", beamVersion)
 
     api("com.google.guava:guava:31.1-jre") {
-        because("Needed by Apache Beam. (https://github.com/GoogleCloudPlatform/cloud-opensource-java/wiki/The-Google-Cloud-Platform-Libraries-BOM#guava-versions--jre-or--android)")
+        because("Needed by Apache Beam. (https://cloud.google.com/java/docs/bom)")
     }
 
     testImplementation("org.apache.beam", "beam-runners-direct-java", beamVersion)

commit d81773fec4efb8877a2a1d7f7b3847fffaf387e7

Use api configuration for logback to fix bug #patch
A previous commit, likely 97f090018f, broke logging in cloud functions.
This change hopefully fixes it.

modules/shared/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
     api("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("ch.qos.logback:logback-classic:1.4.3")
     implementation("org.jetbrains.kotlinx", "kotlinx-serialization-properties")
 
     testImplementation(project(":testing"))
@@ -11,7 +10,7 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jdk8")
     implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310")
 
-    api("org.slf4j:slf4j-api:2.0.3")
+    api("ch.qos.logback:logback-classic:1.4.4")
     api("net.logstash.logback:logstash-logback-encoder:7.2")
     api("org.buildobjects:jproc:2.8.2")
 
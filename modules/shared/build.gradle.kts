836b252b63a38c9ef84c70d2b1ef12a4bbecd9bc
Elias N. Hvideberg <elias.hvideberg@gmail.com>
2022-11-07 15:47:00 +0100

    Downgrade Slf4j and Logback due to Cloud Functions issue #patch
    Issue: https://console.cloud.google.com/support/cases/detail/v2/42068176\?organizationId\=429006794855\&supportedpurview\=project

--- modules/shared/build.gradle.kts
+++ modules/shared/build.gradle.kts
@@ -10,7 +10,10 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jdk8")
     implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310")
 
-    api("ch.qos.logback:logback-classic:1.4.4")
+    // Beware: Don't upgrade to slf4j 2.x until Cloud Function issue is fixed:
+    // https://console.cloud.google.com/support/cases/detail/v2/42068176?organizationId=429006794855&supportedpurview=project
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+    api("org.slf4j:slf4j-api:1.7.36")
     api("net.logstash.logback:logstash-logback-encoder:7.2")
     api("org.buildobjects:jproc:2.8.2")
 

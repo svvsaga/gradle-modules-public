c4c1c6216beb34ba8fb8f0e7be14b1185ed95bdb
Geir Sagberg <geir.sagberg@gmail.com>
2023-01-09 11:42:08 +0100

    Remove ktor-server-1 so we don't have to maintain it anymore #major

--- modules/ktor-server-1/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-val ktorVersion = "1.6.8"
-
-dependencies {
-    implementation(project(":shared"))
-    implementation("io.ktor", "ktor-server-core", ktorVersion)
-
-    testImplementation("io.ktor", "ktor-server-tests", ktorVersion)
-    testImplementation(project(":testing"))
-}

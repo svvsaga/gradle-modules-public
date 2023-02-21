1623ec111c2936111253128116735507e70f17d2
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2023-02-21 15:31:01 +0100

    Switch to require the X-Authorization header for authenticate functions
    As google has begun stripping the signature part of the jwt tokens sent
    in the Authorization header to prevent reuse of tokens provided to
    invoke a function. #major
    
    We actually need the signature part, so we send it in another header.

--- modules/gcp-functions/src/main/kotlin/no/vegvesen/saga/modules/gcp/functions/GoogleUserAuthenticator.kt
+++ modules/gcp-functions/src/main/kotlin/no/vegvesen/saga/modules/gcp/functions/GoogleUserAuthenticator.kt
@@ -15,7 +15,17 @@ class GoogleUserAuthenticator(
     private val tokenProcessor: GoogleIdTokenProcessor = GoogleIdTokenProcessor()
 ) : Logging {
     private fun verifyIdToken(request: HttpRequest): Either<AuthenticationException, IdToken> {
-        val maybeAuthHeader = request.getFirstHeader("Authorization")
+        /**
+         Why not the Authorization header?
+
+         Because the cloud functions runtime have started removing the signature part of the token sent in the Authorization header, to prevent reuse of the tokens that are provided as a function invoker calls a cloud function.
+
+         > To prevent inappropriate re-use of these tokens, Cloud Functions IAM removes the token signature, preventing a function from invoking another function.
+         > https://cloud.google.com/functions/docs/securing/managing-access-iam
+
+         As we actually want the signature part of the token, to verify it was created by google, we send it in another header to prevent the signature from being removed.
+         */
+        val maybeAuthHeader = request.getFirstHeader("X-Authorization")
 
         return if (maybeAuthHeader.isPresent) {
             val header = maybeAuthHeader.get()
@@ -23,15 +33,15 @@ class GoogleUserAuthenticator(
                 val token = header.substring(7)
                 log().info(
                     "Masked bearer token",
-                    kv("masked", "${token.substring(0, 4)}***${token.substring(token.length - 40, token.length)}"),
+                    kv("masked", "${token.substring(0, 4)}***${token.substring(token.length - 3, token.length)}"),
                     kv("length", token.length)
                 )
                 tokenProcessor.parseAndVerify(token)
             } else {
-                AuthenticationException("Unexpected Authorization header format").left()
+                AuthenticationException("Unexpected X-Authorization header format").left()
             }
         } else {
-            AuthenticationException("No Authorization header present").left()
+            AuthenticationException("No X-Authorization header present").left()
         }
     }
 

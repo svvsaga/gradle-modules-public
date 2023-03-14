9ec6d3ad0c9cf6a3e389645965f438f1a5d4046f
Torgeir Thoresen <torgeir.thoresen@vegvesen.no>
2023-03-14 15:26:33 +0100

    Log timing duration even when exceptions are thrown
    #patch

--- modules/shared/src/main/kotlin/no/vegvesen/saga/modules/shared/Timing.kt
+++ modules/shared/src/main/kotlin/no/vegvesen/saga/modules/shared/Timing.kt
@@ -6,16 +6,18 @@ object Timing {
     inline fun <T> withLoggingTimer(logger: Logger, description: String, fn: () -> T): T {
         val start = System.currentTimeMillis()
         logger.info("Timer: '$description', starting.")
-        val result = fn()
-        val end = System.currentTimeMillis()
-        val timeSpent = end - start
-        logger.info(
-            "Timer: '$description', took $timeSpent ms.",
-            kv("unit", "ms"),
-            kv("timer", description),
-            kv("timeSpent", timeSpent)
-        )
-        return result
+        return try {
+            fn()
+        } finally {
+            val end = System.currentTimeMillis()
+            val timeSpent = end - start
+            logger.info(
+                "Timer: '$description', took $timeSpent ms.",
+                kv("unit", "ms"),
+                kv("timer", description),
+                kv("timeSpent", timeSpent)
+            )
+        }
     }
 
     inline fun <T> Logger.measureTime(description: String, fn: () -> T) = withLoggingTimer(this, description, fn)
